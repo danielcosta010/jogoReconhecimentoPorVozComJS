0c934bdd476c0222dcb3a255a4fecdfda123712c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-09-15 02:28:09 -0300

    Função game over
    Adicionado a função game over para finalizar o jogo

--- app/validacao.js
+++ app/validacao.js
@@ -1,7 +1,23 @@
 function verificaSeChutePossuiUmValorValido(chute) {
   const numero = +chute;
 
-  if(ChuteForInvalido(numero)) {
+  if (chuteForInvalido(numero)) {
+    if (chute.toUpperCase() === "GAME OVER") {
+
+        document.body.innerHTML =
+            `
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+    } else {
+
+        elementoChute.innerHTML += '<div>Valor Inválido</div>';
+    }
+  }
+
+  if(chuteForInvalido(numero)) {
     elementoChute.innerHTML += '<div>Valor Inválido</div>';
     return
   }
@@ -28,9 +44,11 @@ function verificaSeChutePossuiUmValorValido(chute) {
     <div>O numero secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>
     `
   }
+  
+
 }
 
-function ChuteForInvalido(numero) {
+function chuteForInvalido(numero) {
   return Number.isNaN(numero);
 }
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
