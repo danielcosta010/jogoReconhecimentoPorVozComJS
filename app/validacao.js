7c7793692bcbfdef9da14442be9c066d8b97da5f
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-09-15 01:51:43 -0300

    Curso reconhecimento por voz com JavaScript

---
+++ app/validacao.js
@@ -0,0 +1,46 @@
+function verificaSeChutePossuiUmValorValido(chute) {
+  const numero = +chute;
+
+  if(ChuteForInvalido(numero)) {
+    elementoChute.innerHTML += '<div>Valor Inválido</div>';
+    return
+  }
+  if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+    elementoChute.innerHTML += `
+    <div>Valor fora do limite: Fale um numero entre ${menorValor} e ${maiorValor}</div>
+    `;
+    return
+  }
+
+  if (numero === numeroSecreto) {
+    document.body.innerHTML = `
+      <h2 class="titulo__tela-acerto">Você Acertou!!!!!!!!</h2>
+      <h3>O numero secreto era ${numeroSecreto}.</h3>
+
+      <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+    `
+  } else if (numero > numeroSecreto) {
+    elementoChute.innerHTML += `
+    <div>O numero secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>
+    `
+  } else {
+    elementoChute.innerHTML += `
+    <div>O numero secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>
+    `
+  }
+}
+
+function ChuteForInvalido(numero) {
+  return Number.isNaN(numero);
+}
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+  return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+  if (e.target.id == 'jogar-novamente') {
+    window.location.reload();
+  }
+})
+
+
